041cd42cae926ecee95dbf97979ba26c31307de9
DjordjeNovcic <32495530+DjordjeNovcic@users.noreply.github.com>
2025-12-21 21:31:58 +0100

    Update firebase.mjs

--- firebase.mjs
+++ firebase.mjs
@@ -1,6 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getDatabase, ref, set, push, onValue }
   from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB9C173giGVH0gt_x4W8f-hoJldHXyRfqo",
@@ -14,9 +20,22 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
+const auth = getAuth(app);
 
+/* EXPORT NA WINDOW (kao i DB) */
 window.db = db;
+window.auth = auth;
+
 window.dbRef = (path) => ref(db, path);
 window.pushTo = (path, data) => push(ref(db, path), data);
 window.setTo = (path, data) => set(ref(db, path), data);
 window.onValue = onValue;
+
+/* AUTH HELPERS */
+window.loginAdmin = (email, password) =>
+  signInWithEmailAndPassword(auth, email, password);
+
+window.logoutAdmin = () => signOut(auth);
+
+window.onAuth = (cb) =>
+  onAuthStateChanged(auth, cb);
